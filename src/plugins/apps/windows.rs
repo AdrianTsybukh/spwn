3bc71094dd8fb0a01328c69456f27210f1808d76
S0RTE0 <sorteosmith@gmail.com>
2026-01-29 20:46:44 +0200

    map windows apps into App model

--- src/plugins/apps/windows.rs
+++ src/plugins/apps/windows.rs
@@ -1,9 +1,22 @@
-use crate::plugins::apps::{App, AppProvider};
+use crate::plugins::{
+    apps::{App, AppProvider},
+};
+use applications::{AppInfo, AppInfoContext};
 
 pub struct WindowsProvider;
 
 impl AppProvider for WindowsProvider {
     fn get_apps(&self) -> Vec<App> {
-        todo!()
+        let mut ctx = AppInfoContext::new(vec![]);
+        ctx.refresh_apps().unwrap();
+        ctx
+            .get_all_apps()
+            .iter()
+            .map(|app| App {
+                name: app.name.clone(),
+                exec_path: app.app_path_exe.clone().unwrap().display().to_string(),
+                icon_name: None,
+            })
+            .collect()
     }
 }
--- src/plugins/shell.rs
+++ src/plugins/shell.rs
@@ -23,6 +23,28 @@ impl Plugin for Shell {
     }
 }
 
+
+#[cfg(target_os = "windows")]
+pub async fn run_command_logic(input: String) -> Result<String, String> {
+    let output = tokio::process::Command::new("cmd")
+        .args(["/C", &input])
+        .output()
+        .await
+        .map_err(|e| e.to_string())?;
+
+    if output.status.success() {
+        Ok(String::from_utf8_lossy(&output.stdout).to_string())
+    } else {
+        let err = String::from_utf8_lossy(&output.stderr).to_string();
+        if err.trim().is_empty() {
+            Err(format!("Error: Command failed with status {:?}", output.status.code()))
+        } else {
+            Err(err)
+        }
+    }
+}
+
+#[cfg(target_os = "linux")]
 pub async fn run_command_logic(input: String) -> Result<String, String> {
     let args = shell_words::split(&input)
         .map_err(|e| format!("Parse error: {}", e))?;
